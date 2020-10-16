d1b3b48229f5153d4fb9df3cab48999c4548bc13
Anton Kurdin <anton.kurdin@gmail.com>
2020-10-16 16:43:28 +0300

    native test-redux-thunk

--- native/App.js
+++ native/App.js
@@ -1,15 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect} from 'react';
+import { Provider } from "react-redux";
+import { store } from './src/redux/store'
+import { useDispatch, useSelector } from 'react-redux';
+import { getTestText } from './src/redux/actions';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
+const WithRedux = (Component) => {
+  return (props) => {
+    return <Provider store={store}>
+    <Component {...props}/>
+    </Provider>
+  }
+}
+
+function App() {
+
+  const test = useSelector(state => state.test)
+  const loading = useSelector(state => state.loadingTest)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTestText())
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Oh yeeeeah, RAMA-boys00034!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <View style={styles.container}>
+          <Text>{loading && 'loading...'}</Text>
+          <Text>{test && test}</Text>
+          {/* <Image source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}} />  */}
+          <StatusBar style="auto" />
+        </View>
   );
 }
+
+export default WithRedux(App)
 
 const styles = StyleSheet.create({
   container: {
---
+++ native/src/redux/actionTypes.js
@@ -0,0 +1,2 @@
+export const LOADING_TEST = 'LOADING_TEST'
+export const GET_TEST_TEXT = 'GET_TEST_TEXT'
