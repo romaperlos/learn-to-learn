d8d39278bd8651e5c1e46989dcfc9c5ff5fe5c8e
Max <mvyakovlev@outlook.com>
2020-10-21 18:28:19 +0300

    upload1

--- server/routes/company.js
+++ server/routes/company.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import multer from 'multer';
+import storage from '../middleware/upload';
 import can from 'canvas';
 import FastAverageColor from 'fast-average-color';
 import Company from '../models/Company.js';
 
 const { loadImage, createCanvas } = can;
+
+const upload = multer({ storage: storage }).single('file');
 
 const router = express.Router();
 
@@ -52,5 +56,24 @@ router.post('/', async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => { cb(null, 'public') },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+});
+
+router.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).send(req.file);
+  });
+});
+
 
 export default router;
