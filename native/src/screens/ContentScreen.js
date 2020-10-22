6175ed0822c637fa45456259a04226a4495a625f
Anton Kurdin <anton.kurdin@gmail.com>
2020-10-22 12:54:26 +0300

    show content in process

--- native/src/screens/ContentScreen.js
+++ native/src/screens/ContentScreen.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
 import {
-  View, Text, StyleSheet, FlatList,
+  View, Text, StyleSheet, FlatList, ScrollView,
 } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -13,7 +13,6 @@ import { VideoURL } from '../components/VideoURL';
 import { TextURL } from '../components/TextURL';
 import { TextArea } from '../components/TextArea';
 import { PicURL } from '../components/picURL';
-
 
 export function ContentScreen({ navigation }) {
   const subDirectory = navigation.getParam('subDirectory');
@@ -31,15 +30,15 @@ export function ContentScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.topMenu}>
+      {/* <View style={styles.topMenu}>
         <FlatList
           data={subDirectoryContent}
           keyExtractor={(contentTitle) => contentTitle._id.toString()}
           renderItem={({ item }) => <ContentTitle contentTitle={item} onShowContent={showContent} />}
-        />
+        /> */}
 
-      </View>
-      <View style={styles.contentContainer}>
+      {/* </View> */}
+      {/* <View style={styles.contentContainer}>
         <FlatList
           data={subDirectoryContent[0].item}
           keyExtractor={(contentItem) => contentItem.id.toString()}
@@ -54,9 +53,50 @@ export function ContentScreen({ navigation }) {
               return <TextArea textArea={item.value} />;
             } else if (item.type === 'picUrl') {
               return <PicURL picUrl={item.value} />;
-          }}}
-        />
-      </View>
+            }
+          }}
+        /> */}
+      <FlatList
+        data={subDirectoryContent}
+        keyExtractor={(contentTitle) => contentTitle._id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+            {item.item.map((el) => {
+              if (el.type === 'subtitle') {
+                return <Subtitle subtitle={el.value} />;
+              } else if (el.type === 'videoUrl') {
+                  return <VideoURL videoURL={el.value} />;
+                } else if (el.type === 'link') {
+                  return <TextURL textURL={el.value} />;
+                } else if (el.type === 'textArea') {
+                  return <TextArea textArea={el.value} />;
+                } else if (el.type === 'picUrl') {
+                  return <PicURL picUrl={el.value} />;
+                }
+            })}
+            {/* <FlatList
+              data={item.item}
+              keyExtractor={(contentItem) => Date.now().toString()}
+              renderItem={({ contentItem }) => {
+                if (contentItem.type === 'subtitle') {
+                  return <Subtitle subtitle={contentItem.value} />;
+                } else if (contentItem.type === 'videoUrl') {
+                  return <VideoURL videoURL={contentItem.value} />;
+                } else if (contentItem.type === 'link') {
+                  return <TextURL textURL={contentItem.value} />;
+                } else if (contentItem.type === 'textArea') {
+                  return <TextArea textArea={contentItem.value} />;
+                } else if (contentItem.type === 'picUrl') {
+                  return <PicURL picUrl={contentItem.value} />;
+                }
+              }}
+            /> */}
+          </View>
+        )}
+      />
+
+      {/* </View> */}
     </View>
   );
 }
@@ -80,23 +120,23 @@ ContentScreen.navigationOptions = ({ navigation }) => ({
 });
 
 const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 15,
-  },
-  topMenu: {
-    width: '100%',
-    height: 'auto',
-    marginTop: 30,
-    paddingHorizontal: 10,
-    borderBottomWidth: 1,
-    borderColor: '#D8D8D8',
-  },
-  contentContainer: {
+  // container: {
+  //   width: '100%',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   paddingTop: 15,
+  // },
+  // topMenu: {
+  //   width: '100%',
+  //   height: 'auto',
+  //   marginTop: 30,
+  //   paddingHorizontal: 10,
+  //   borderBottomWidth: 1,
+  //   borderColor: '#D8D8D8',
+  // },
+  // contentContainer: {
 
-    marginHorizontal: 5,
-    // alignItems: 'center',
-  },
+  //   marginHorizontal: 5,
+  //   // alignItems: 'center',
+  // },
 });
