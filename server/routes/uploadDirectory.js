ba94abbfb96f976966e8d13815bbe59f25316522
Max <mvyakovlev@outlook.com>
2020-10-23 13:21:38 +0300

    release PWD

--- server/routes/uploadDirectory.js
+++ server/routes/uploadDirectory.js
@@ -7,26 +7,23 @@ const router = express.Router();
 
 const multerStorage = multer.diskStorage({
   destination(req, file, callback) {
-    console.log(process.cwd(), '<<<<<<<<');
-    callback(null, './uploadFiles/directory');
+    callback(null, '../public/uploadFiles/directory/');
   },
   filename(req, file, callback) {
-    console.log(file);
-    // const fileExtension = file.originalname.match(/\.[^.]+$/m)[0];
-    // console.log(fileExtension);
-    // const fileName = `${file.fieldname.slice(0, -fileExtension.length)}_${uuidv4()}${fileExtension}`;
-    callback(null, file.originalname.replace(/(\.[^.]+)$/, '_' + uuidv4() + '$1'));
+    callback(null, file.originalname.replace(/(\.[^.]+)$/, `_${uuidv4()}$1`));
   },
 });
 
 const upload = multer({ storage: multerStorage });
 
-
 router.post('/', (req, res, next) => {
-  console.log('>>>>>>>>>>>>>>>>>', req.body);
   next();
 }, upload.single('file'), (req, res) => {
-  console.log('>>>>>>>>>>>>>>>>>', req.file);
+  const reqBody = { ...req.body };
+  const { directoryId } = reqBody;
+  const way = req.file.path;
+  const { filename } = req.file;
+
   res.json({ responseText: 'Response from file upload!' });
 });
 
