26936aff1399cec926e14df22eed0d567789deaa
Max <mvyakovlev@outlook.com>
2020-10-23 10:42:02 +0300

    loading done

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,24 +1,30 @@
 import express from 'express';
 import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
 
 const router = express.Router();
 
 const multerStorage = multer.diskStorage({
   destination(req, file, callback) {
+    console.log(process.cwd(), '<<<<<<<<');
     callback(null, './uploadFiles');
   },
   filename(req, file, callback) {
-    const fileExtension = file.fieldname.match(/\.[^.\\/:*?"<>|\r\n]+$/m)[0];
-    console.log(fileExtension);
-    const fileName = `${file.fieldname.slice(0, -fileExtension.length)}_${uuidv4()}${fileExtension}`;
-    callback(null, fileName);
+    console.log(file);
+    // const fileExtension = file.originalname.match(/\.[^.]+$/m)[0];
+    // console.log(fileExtension);
+    // const fileName = `${file.fieldname.slice(0, -fileExtension.length)}_${uuidv4()}${fileExtension}`;
+    callback(null, file.originalname.replace(/(\.[^.]+)$/, '_' + uuidv4() + '$1'));
   },
 });
 
 const upload = multer({ storage: multerStorage });
 
-router.post('/', upload.any(), (req, res) => {
+router.post('/', (req, res, next) => {
+  console.log('>>>>>>>>>>>>>>>>>', req.body);
+  next();
+}, upload.single('file'), (req, res) => {
   res.json({ responseText: 'Response from file upload!' });
 });
 
